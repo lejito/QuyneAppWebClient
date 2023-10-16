1cea6ee1a6132e5003740c8a09344ca3d041021f
olarteviviana <olarteviviana.a@gmail.com>
2023-10-16 01:27:37 -0500

    feat: función DeleteP

--- pages/bolsillo.vue
+++ pages/bolsillo.vue
@@ -2,76 +2,76 @@
     <div class="container">
         <div class="contenedor">
             <v-container class="card">
-                <NuxtLink to="/inicio">
-                    <v-icon class="lr-card__back" icon="mdi-arrow-left" size="large"></v-icon>
-                </NuxtLink>
-                <v-row justify="center">
-                    <v-col cols="1" style="margin-right: 10px;">
-                        Bolsillos
-                    </v-col>
-                </v-row>
+                <v-card class="centered-content">
+                    <NuxtLink to="/inicio">
+                        <v-icon class="lr-card__back" icon="mdi-arrow-left" size="large"></v-icon>
+                    </NuxtLink>
 
-                <v-row justify="center">
-                    <v-col cols="9">
-                        <div v-if="bolsillos.length > 0" v-for="item in bolsillos" :key="item.id">
-                            <v-banner lines="one">
+                    <v-row justify="center">
+                        <v-col cols="9">
+                            <div v-if="bolsillos.length > 0" v-for="item in bolsillos" :key="item.id">
+                                <v-banner lines="one">
 
 
-                                <template v-slot:text>{{ item.nombre }}
-                                    $ {{ item.saldo_disponible }}
-                                </template>
-                                <template v-slot:actions>
-                                    <v-btn>
-                                        Editar
-                                    </v-btn>
-                                    <v-btn>
-                                        Transferir
-                                    </v-btn>
-                                    <v-btn @click="deleteBolsillo(item)">
-                                        Eliminar
-                                    </v-btn>
-                                </template>
-                            </v-banner>
+                                    <template v-slot:text>{{ item.nombre }}
+                                        $ {{ item.saldo_disponible }}
+                                    </template>
+                                    <template v-slot:actions>
+                                        <v-btn color="var(--color-secondary)">
+                                            Editar
+                                        </v-btn>
+                                        <v-btn color="var(--color-primary)">
+                                            Transferir
+                                        </v-btn>
+                                        <v-btn color="var(--color-danger)" @click="DeleteP(item)">
+                                            Eliminar
+                                        </v-btn>
+                                    </template>
+                                </v-banner>
+                            </div>
+                            <p v-else>
+                                Actualmente no tienes ningún bolsillo activo
+                            </p>
+                        </v-col>
+                    </v-row>
+                    <Transition name="fade">
+                        <div class="modal-overlay" v-if="showModal"></div>
+                    </Transition>
+                    <Transition name="fade">
+                        <div class="modal" v-if="showModal">
+                            <v-card>
+                                <v-card-title>Personaliza tu bolsillo</v-card-title>
+                                <v-card-text>
+                                    <div class="lr-form__group">
+                                        <input type="text" name="nombreBolsillo" id="nombreBolsillo" v-model="newPocketName"
+                                            placeholder="Nombre del bolsillo (Obligatorio)" class="lr-form__input">
+                                    </div>
+                                    <br>
+                                    <div class="lr-form__group">
+                                        <input type="number" name="meta" id="meta" v-model="newPocketObjetivo"
+                                            placeholder="¿Cuánto necesitas para alcanzar tu meta? (Opcional)"
+                                            class="lr-form__input">
+                                    </div>
+                                </v-card-text>
+                                <v-card-actions>
+                                    <v-spacer></v-spacer>
+                                    <v-btn class="botonG" @click="CreatePocket">Crear Bolsillo</v-btn>
+                                    <v-btn class="botonC" @click="showModal = false">Cancelar</v-btn>
+                                </v-card-actions>
+                            </v-card>
                         </div>
-                        <p v-else>
-                            Actualmente no tienes ningún bolsillo activo
-                        </p>
-                    </v-col>
-                </v-row>
-                <Transition name="fade">
-                    <div class="modal-overlay" v-if="showModal"></div>
-                </Transition>
-                <Transition name="fade">
-                    <div class="modal" v-if="showModal">
-                        <v-card>
-                            <v-card-title>Personaliza tu bolsillo</v-card-title>
-                            <v-card-text>
-                                <div class="lr-form__group">
-                                    <input type="text" name="nombreBolsillo" id="nombreBolsillo" v-model="newPocketName"
-                                        placeholder="Nombre del bolsillo (Obligatorio)" class="lr-form__input">
-                                </div>
-                                <br>
-                                <div class="lr-form__group">
-                                    <input type="number" name="meta" id="meta" v-model="newPocketObjetivo"
-                                        placeholder="¿Cuánto necesitas para alcanzar tu meta? (Opcional)"
-                                        class="lr-form__input">
-                                </div>
-                            </v-card-text>
-                            <v-card-actions>
-                                <v-spacer></v-spacer>
-                                <v-btn color="primary" @click="CreatePocket">Crear Bolsillo</v-btn>
-                                <v-btn color="error" @click="showModal = false">Cancelar</v-btn>
-                            </v-card-actions>
-                        </v-card>
-                    </div>
-                </Transition>
-                <v-row justify="center">
-                    <v-col cols="3">
-                        <button @click="showModal = true" class="index-navbar__options__button">
+                    </Transition>
+                    <v-row justify="end" class="mb-3">
+                        <v-col cols="3">
+                            <v-btn
+                                class="boton1"
+                                @click="showModal = true"
+                            >
                             Crear Bolsillo
-                        </button>
-                    </v-col>
-                </v-row>
+                            </v-btn>
+                        </v-col>
+                    </v-row>
+                </v-card>
             </v-container>
         </div>
     </div>
@@ -109,13 +109,13 @@ async function CreatePocket() {
     showModal.value = false
 }
 
+async function DeleteP(bolsilloToDelete) {
+    await BolsilloService.deleteBolsillo(bolsilloToDelete, bolsillos);
+}
+
 definePageMeta({
     layout: "navbar"
 });
-async function deleteBolsillo(item) {
-
-    await BolsilloService.deleteBolsillo(item, bolsillos);
-}
 </script>
 
 <style>
@@ -145,5 +145,49 @@ async function deleteBolsillo(item) {
     border-radius: 15px;
     box-shadow: 3px 3px rgba(0, 0, 0, 0.4);
     z-index: 101;
+}
+
+.centered-content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 10px;
+  margin-top: 70px;
+}
+
+.boton1 {
+  background-color: transparent;
+  border: 4px solid var(--color-terciario);
+  color: var(--color-terciario);
+  padding: 10px 20px;
+  border-radius: 30px;
+  transition: background-color 0.3s, color 0.3s, box-shadow 0.3s;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  font-weight: bold;
+
+}
+
+.botonC {
+  background-color: transparent;
+  border: 4px solid var(--color-danger);
+  color: var(--color-danger);
+  padding: 10px 20px;
+  border-radius: 30px;
+  transition: background-color 0.3s, color 0.3s, box-shadow 0.3s;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  font-weight: bold;
+
+}
+
+.botonG {
+  background-color: transparent;
+  border: 4px solid var(--color-success);
+  color: var(--color-success);
+  padding: 10px 20px;
+  border-radius: 30px;
+  transition: background-color 0.3s, color 0.3s, box-shadow 0.3s;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  font-weight: bold;
+
 }
 </style>
