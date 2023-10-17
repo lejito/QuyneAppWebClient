5741dddf577d207248a7924da8021fdb7a4b640b
Lejito <alejocrrs@gmail.com>
2023-10-17 00:20:53 -0500

    Change APIURL to the remote route

--- services/Global.js
+++ services/Global.js
@@ -1,4 +1,3 @@
 export const Global = {
-  APIURL: "http://localhost:3000/api"
-  //APIURL: "https://quyneappwebserver-140x-dev.fl0.io/api"
+  APIURL: "https://quyneappwebserver-140x-dev.fl0.io/api"
 }
