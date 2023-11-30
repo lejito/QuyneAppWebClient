f4aade94703d105499328ef75de810cdb51784f9
Lejito <alejocrrs@gmail.com>
2023-11-29 20:41:58 -0500

    Change the api url and readme generated by Nuxt

--- services/Global.js
+++ services/Global.js
@@ -1,4 +1,4 @@
 export const Global = {
-  APIURL: "https://quyneappwebserver-dev-tjrs.4.us-1.fl0.io/api",
-  // APIURL: "http://localhost:3000/api",
+  APIURL: "http://localhost:3000/api",
+  //APIURL: "https://quyneappwebserver-dev-tjrs.4.us-1.fl0.io/api",
 };
